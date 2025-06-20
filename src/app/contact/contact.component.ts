20e07d8ea5c874a2ea0570499ead1cfebd80fae3
Devdot05 <olawoledotun85@gmail.com>
2025-06-20 14:54:47 +0100

    updated code

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,6 +3,9 @@ import { MessageService } from '../services/message.service';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {MatSnackBar} from '@angular/material/snack-bar'
+import emailjs from '@emailjs/browser';
+
+
 import { SnackbarComponent } from '../snackbar/snackbar.component';
 
  
@@ -28,32 +31,62 @@ export class ContactComponent {
     private _message: MessageService
   ) {}
 
-  message = "please fill all the required field"
-  contactMe () {
-  
-    console.log(this.userForm.value);
-    if(this.userForm.valid ){
-      this._snackBar.openFromComponent( SnackbarComponent, {
-        duration: 3000,
-        horizontalPosition: 'center',
-        verticalPosition: 'bottom'
-      })
-    }else{
-      this._snackBar.open( this.message)
-    }
+  // message = "please fill all the required field"
+  // contactMe () {
+  //   console.log(this.userForm.value);
+  //   if(this.userForm.valid ){
+  //     this._snackBar.openFromComponent( SnackbarComponent, {
+  //       duration: 3000,
+  //       horizontalPosition: 'center',
+  //       verticalPosition: 'bottom'
+  //     })
+  //   }else{
+  //     this._snackBar.open( this.message)
+  //   }
     
-    this._message.messageMe(this.userForm.value).subscribe({
-      next: (res) => {
-        console.log(res);
+  //   this._message.messageMe(this.userForm.value).subscribe({
+  //     next: (res) => {
+  //       console.log(res);
        
         
-      },
-      error: (err) => {
-        console.log(err);
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
        
-      }
-    })
+  //     }
+  //   })
+  // }
+
+  contactMe() {
+  if (this.userForm.invalid) {
+    this.userForm.markAllAsTouched();
+ 
+    return;
   }
+
+  this._snackBar.openFromComponent( SnackbarComponent, {
+    duration: 3000,
+    horizontalPosition: 'center',
+    verticalPosition: 'bottom'
+    })
+  
+
+  const templateParams = {
+    user_name: this.userForm.value.full_name,
+    user_email: this.userForm.value.email,
+    feedback_message: this.userForm.value.message
+  };
+
+  emailjs.send('service_jktolhl', 'template_352v14r', templateParams, 'O2YDSU6-TpYLV1ryz')
+    .then(response => {
+      console.log('SUCCESS!', response, response.text);
+    })
+    .catch(error => {
+      console.error('FAILED...', error);
+    });
+  }
+
 
    
 }
+
